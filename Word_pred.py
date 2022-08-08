bc7653b904b24f0789a6ab43493afec8e6e99b39
Akhil Mathew <80476928+AkhilMathew1110@users.noreply.github.com>
2022-08-08 01:12:11 -0400

    Update Word_pred.py

--- Word_pred.py
+++ Word_pred.py
@@ -130,7 +130,7 @@ if authentication_status:
 
   if st.button('Predict'):
       st.text_area("Predicted List is Here",answer_as_string,key="predicted_list")
-      #option=st.selectbox("Predicted List is Here",answer_as_string,key="predicted_list")
+      option=st.selectbox("Predicted List is Here",answer_as_string,key="predicted_list")
       #for i in option:
        #st.write(input_text,option(i))
   st.image('https://imageio.forbes.com/blogs-images/cognitiveworld/files/2019/06/types-of-AI.jpg?format=jpg&width=960',use_column_width=True)
