6fbd1bc01988e693ee1abe9e7a3a071478eeb40a
Akhil Mathew <80476928+AkhilMathew1110@users.noreply.github.com>
2022-08-08 01:50:56 -0400

    Update Word_pred.py

--- Word_pred.py
+++ Word_pred.py
@@ -131,7 +131,8 @@ if authentication_status:
 
   if st.button('Predict'):
       st.text_area("Predicted List is Here",answer_as_string,key="predicted_list")
-      #option=st.selectbox("Predicted List is Here",answer_as_string,key="predicted_list")
+      option=st.selectbox("Predicted List is Here",answer_as_string,key="predicted_list")
+      st.write({input_text} {option})
   st.image('https://imageio.forbes.com/blogs-images/cognitiveworld/files/2019/06/types-of-AI.jpg?format=jpg&width=960',use_column_width=True)
 
  except Exception as e:
